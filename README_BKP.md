e7669f1fdb4d3df89e2d3c2b7aada47e7feaef48
William Alexander Pacheco Valderrama <wpachecov@unal.edu.co>
2024-05-12 18:28:59 -0500

    Steps 1 and 2.
    I created a profile introduction in the README_BKP file, and I edited the existing hello.py file with the solution code to step 2.

---
+++ README_BKP.md
@@ -0,0 +1,15 @@
+
+# A-guacate
+
+William Alexander Pacheco Valderrama
+
+![Universe banner](/Images/fea5a2ebb5407dfeba6c9c55e5207c40.jpg)
+
+> No buses | Arctic Monkeys [▶](https://open.spotify.com/track/6mOiT65GFmXgBAviM3lrsE)
+
+- 🌎 Colombia
+- 🪐 Physics student at Universidad Nacional de Colombia
+- 📩 wpachecov@gmail.com
+- 🌙 Crossfit athlete and ocasional musician 
+
+
--- hello.py
+++ hello.py
@@ -1,2 +1,19 @@
 def hello_world():
-	return "Hello"
+	return "Hello World!"
+
+def hello_world_n(N):
+    result = ""
+    for _ in range(N):
+        if _ < N - 1:
+            result += "Hello World! "
+        else:
+            result += "Hello World!"
+    return result
+
+
+
+## Mini test
+# result1 = hello_world()
+# print(result1)
+# result2 = hello_world_n(5)
+# print(result2)
